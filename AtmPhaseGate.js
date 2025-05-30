52d933284ea87fb4dd9b86cd623698194b00c51b
lotannauba <lotannauba23@gmail.com>
2025-05-30 12:02:26 +0100

    Add files via upload

---
+++ AtmPhaseGate.js
@@ -0,0 +1,48 @@
+let array = [4,8,4,6,0,8,9,0,9,4,0,4,9,0,4,0];
+	array.each((element))=> {
+		console.log(element);
+	if(array.length == 16 && array Index == 4){
+			};
+		else{
+		console.log(invalid length);
+			};
+};
+
+let Masterarray = [5,8,4,6,0,8,9,0,9,4,0,4,9,0,4,0];
+	array.each((element))=> {
+		console.log(element);
+	if(array.length ==16 && array Index == 5){
+			};
+		else{
+		console.log(invalid length)
+			};
+
+};
+
+let array = [6,8,4,6,0,8,9,0,9,4,0,4,9,0,4,0];
+	array.each((element))=> {
+		console.log(element);
+	if(array.length ==16 && array Index ==6){
+			};
+};  
+		else{
+		console.log(invalid length)
+			};
+		
+
+let array = [3,8,4,6,0,8,9,0,9,8,4,9,0,4,0];
+	array.each((element))=> {
+		console.log(element);
+
+	if(array.length == 15 && array Index ==3){
+			};
+
+};		else{
+		console.log(element);
+			};
+
+
+
+
+
+                                           
