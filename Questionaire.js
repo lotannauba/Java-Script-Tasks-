6c9d2c86baab48b2e2c75951409519519f23aa52
lotannauba <lotannauba23@gmail.com>
2025-06-05 16:47:55 +0100

    Add files via upload

---
+++ Questionaire.js
@@ -0,0 +1,94 @@
+const prompt = require("prompt-sync")();
+
+let score = prompt(" -------------------WELCOME TO MY QUESTIONAIRE--------------------------");
+
+
+
+
+
+const prompt1 = require("prompt-sync")();
+
+let Question1 = prompt1 ("How many Champions league does Arsenal have?: ");
+	console.log("A: 3, B: 15, C: 9, D: 0 ")
+console.log(Question1);
+		console.log("incorrect Answer❌")
+
+
+
+const prompt2 = require("prompt-sync")();
+
+let Question2 = prompt2 (" How many goals does Ronaldo have this season?: ");
+	console.log("A: 70, B: 455, C: 300, D: 1000")
+console.log(Question2);
+		console.log("correct Answer☑️")
+
+
+
+const prompt3 = require("prompt-sync")();
+
+let Question3 = prompt3 (" How many champions league does Manunited have?: ");
+	console.log("A: 3, B: 15, C: 9, D: 0 ")
+console.log(Question3);
+		console.log("Incorrecrt Answer❌")
+
+
+
+const prompt4 = require("prompt-sync")();
+
+let Question4 = prompt4 ("How old is cole palmer?");
+	console.log("A: 3, B: 22, C: 9, D: 0 ")
+console.log(Question4);
+		console.log("Correct Answer☑️")
+
+
+const prompt5 = require("prompt-sync")();
+
+let Question5 = prompt5 ("Who is the best player in the premier league? ");
+	console.log("A: 3, B: 15, C: 9, D: 0 ")
+console.log(Question5);
+		console.log("Incorrect Answer❌")
+
+
+
+const prompt6 = require("prompt-sync")();
+
+let Question6 = prompt6 ("who is the best player in chelsea? ");
+	console.log("A: cole palmer , B: Noni madueke , C: Enzo Fernandez, D: Colwill ")
+console.log(Question6);
+		console.log("Correct Answer☑️")
+
+
+
+const prompt7 = require("prompt-sync")();
+
+let Question7 = prompt7 ("how many mm makes one metre? ");
+	console.log("A: 0.5, B: 0.66, C: 9.0 , D: 4.6 ")
+console.log(Question7);
+		console.log("Correct Answer☑️")
+
+
+
+const prompt8 = require("prompt-sync")();
+
+let Question8 = prompt8 ("How many continents do we have in the world? ");
+	console.log("A: 3, B: 15, C: 8, D: 0 ")
+console.log(Question8);
+		console.log("correct answer☑️")
+
+
+
+const prompt9 = require("prompt-sync")();
+
+let Question9 = prompt9 ("how long is a flight from Murtala  mohammed airport to heathrow Airport? ");
+	console.log("A: 3, B: 15, C: 6, D: 0 ")
+console.log(Question9);
+		console.log("Correct Answer☑️")
+
+
+
+const prompt10 = require("prompt-sync")();
+
+let Question10 = prompt10 ("How many goals does Matheus cunha have this season?");
+	console.log("A: 3, B: 15, C: 21, D: 0 ")
+console.log(Question10);
+		console.log("Correct Answer☑️")
